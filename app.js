8633fc07a960cb69efca5074b7c1fa3de35b1027
Aryan Manhas <2022A1R033@mietjammu.in>
2025-09-22 02:06:56 +0530

    app.js

--- app.js
+++ app.js
@@ -27,43 +27,99 @@ app.get("/login" , (req , res)=>{
 
 app.get("/logout" , (req , res)=>{
     res.cookie("token" , "")
-    res.redirect("/login")
+    res.redirect("/login") 
 })
 
 
-app.post("/register" , async(req , res)=>{
-    let {email , password , username , name , age} = req.body
-    let user = await userModel.findOne({email})
-    if (user) return res.status(500).send("User Already Registered")
+// app.post("/register" , async(req , res)=>{
+//     let {email , password , username , name , age} = req.body
+//     let user = await userModel.findOne({email})
+//     if (user) return res.status(500).send("User Already Registered")
 
-    bcrypt.genSalt(10 , async (err , salt)=>{
-        let newUser = await bcrypt.hash(password , salt , (err , hash)=>{
-            usertModel.create({
-                username, 
-                email,
-                age,
-                name,
-                password : hash
-            })
+//     bcrypt.genSalt(10 , async (err , salt)=>{
+//         let user = await bcrypt.hash(password , salt , (err , hash)=>{
+//             usertModel.create({
+//                 username, 
+//                 email,
+//                 age,
+//                 name,
+//                 password : hash
+//             })
             
-        })
-    })
+//         })
+//     })
     
-    let token = jwt.sign({email : email , userid : newUser._id} , "shhhh");
-    res.cookie("token" , token);
-    res.send("registered")
-})
+//     let token = jwt.sign({email : email , userid : user._id} , "shhhh");
+//     res.cookie("token" , token);
+//     res.send("registered")
+// })
 
-app.post("/login" , async(req , res)=>{
-    let {email , password} = req.body
-    let user = await userModel.findOne({email})
-    if (!user) return res.status(500).send("Something Went Wrong!")
+app.post("/register", async (req, res) => {
+  try {
+    let { email, password, username, name, age } = req.body;
 
-        bcrypt.compare(password , user.password , (err , result)=>{
-            if(result) res.status(200).send("Logged In")
-                else(res.redirect("/Login"))
-        })
-})
+    let user = await userModel.findOne({ email });
+    if (user) return res.status(400).send("User Already Registered");
+
+    // hash password properly
+    const hash = await bcrypt.hash(password, 10);
+
+    const newUser = await userModel.create({
+      username,
+      email,
+      age,
+      name,
+      password: hash,
+    });
+
+    // create token
+    let token = jwt.sign({ email: newUser.email, userid: newUser._id }, "shhhh");
+
+    // set cookie BEFORE sending response
+    res.cookie("token", token);
+    res.send("registered");
+  } catch (err) {
+    console.error("Register error:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+// app.post("/login" , async(req , res)=>{
+
+app.post("/login", async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    let user = await userModel.findOne({ email });
+    if (!user) return res.status(400).send("Invalid Email or Password");
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).send("Invalid Email or Password");
+
+    // create token
+    let token = jwt.sign({ email: user.email, userid: user._id }, "shhhh");
+
+    // set cookie BEFORE response
+    res.cookie("token", token);
+    res.send("Logged In");
+  } catch (err) {
+    console.error("Login error:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+
+//     let {email , password} = req.body
+//     let user = await userModel.findOne({email})
+//     if (!user) return res.status(500).send("Something Went Wrong!")
+
+//         bcrypt.compare(password , user.password , (err , result)=>{
+//             if(result) {res.status(200).send("Logged In")
+//                 let token = jwt.sign({email : email , userid : user._id} , "shhhh");
+//                 res.cookie("token" , token);}
+//                 else(res.redirect("/Login"))
+//         })
+// })
 
 
 //     let token = jwt.sign({email : email , userid : newUser._id} , "shhhh");
@@ -111,6 +167,31 @@ app.post("/login" , async(req , res)=>{
 //     return res.status(500).send("Internal Server Error");
 //   }
 // });
+
+//middleware for login information
+
+// function isLoggedIn(req , res , next){
+//     if (req.cookie.token === "" ) res.send("You must be Logged In")
+//         else{
+//             let data = jwt.verify(req.cookie.token , "shhhh")
+//             req.user = data;
+//         }
+//     next();
+//     }
+
+function isLoggedIn(req, res, next) {
+  const token = req.cookies.token;
+  if (!token) return res.send("You must be Logged In");
+
+  try {
+    let data = jwt.verify(token, "shhhh");
+    req.user = data;
+    next();
+  } catch (err) {
+    return res.send("Invalid Token");
+  }
+}
+
 
 
 
